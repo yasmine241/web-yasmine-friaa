a49be2063a4133c105cb4c653bb654671e2df22e
yasmine241 <friayassmine@gmail.com>
2026-06-10 11:45:21 +0200

    login feature

--- Frontend/assets/js/config.js
+++ Frontend/assets/js/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "http://localhost:5000";
 
 function getToken()      { return localStorage.getItem("token"); }
 function setToken(token) { localStorage.setItem("token", token); }
